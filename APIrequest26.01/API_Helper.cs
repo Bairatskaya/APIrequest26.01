a07da8418ce5818a7dba4abaf0b838845de6635d
Bairatskaya <anastasiya.bairatskaya@gmail.com>
2022-01-28 12:34:44 +0200

    Добавьте файлы проекта.

---
+++ APIrequest26.01/API_Helper.cs
@@ -0,0 +1,45 @@
+﻿using RestSharp;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+
+
+namespace APIrequest26._01
+{
+    public class API_Helper
+    {
+        public IRestResponse SendApiRequest(object body,Dictionary<string,string> headers,string link,Method type)
+        {
+            RestClient client = new RestClient(link)
+            {
+                Timeout = 10000
+            };
+            RestRequest request = new RestRequest(Method.POST);
+            foreach (var header in headers)
+            request.AddHeader(header.Key, header.Value);
+            request.AddJsonBody(body);
+            request.RequestFormat = DataFormat.Json;
+
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+        public Cookie ExtractCookie(IRestResponse response, string cookieName)
+        {
+            Cookie result = null;
+            foreach (var cookie in response.Cookies)
+                if (cookie.Name.Equals(cookieName))
+                    result = new Cookie(cookie.Name, cookie.Value, cookie.Domain, cookie.Path, null);
+            return result;
+        }
+        public static byte[] GetPicRequest(string picUrl)
+        {
+            var client = new RestClient(picUrl);
+            var request = new RestRequest(Method.GET);
+            byte[] picInByte = client.DownloadData(request);
+            return picInByte;
+        }
+
+
+    }
+}
---
+++ APIrequest26.01/UnitTest1.cs
@@ -0,0 +1,62 @@
+using RestSharp;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using System.IO;
+
+namespace APIrequest26._01
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var body = new Dictionary<string, string>
+            {
+                {"l_","12301@gmail.com"},
+                {"pw_","123"}
+
+            };
+            var headers = new Dictionary<string, string>
+            {
+                {"Content-Type","application/javascript"}
+            };
+
+            var response = new API_Helper().SendApiRequest(body, headers, "https://ek.ua/", RestSharp.Method.POST);
+            var cookie = new API_Helper().ExtractCookie(response, "PHPSESSID");
+            //var cookie2 = new API_Helper().ExtractCookie(response, "access_token");
+
+            IWebDriver chrome = new ChromeDriver();
+            chrome.Navigate().GoToUrl("https://ek.ua/");
+            System.Threading.Thread.Sleep(10000);
+
+            chrome.Manage().Cookies.AddCookie(cookie);
+            //chrome.Manage().Cookies.AddCookie(cookie2);
+
+            chrome.Navigate().GoToUrl("https://ek.ua/");
+            System.Threading.Thread.Sleep(5000);
+            //chrome.Close();
+        }
+        [Fact]
+        public void DownloudPic()
+        {
+            byte[] content = API_Helper.GetPicRequest("https://i.pinimg.com/originals/e8/9a/ae/e89aae77cead87e332063ce9a9140d0e.jpg");
+            File.WriteAllBytes(Path.Combine("/Picture", "picture.jpg"), content);
+        }
+
+        [Fact]
+        public void UploadPic()
+        {
+        var client = new RestClient("https://radikal.ru/");
+        client.Timeout = -1;
+var request = new RestRequest(Method.POST);
+        request.AddHeader("Cookie", "SID=8c5c54284eb3463d83d067386c331b55; UID=90dfa8fea9d0490e95c8a5751dd8b4ce");
+request.AddFile("picture", "/Picture/krasivye-pupsiki-shchenochki-pomeranskogo-shpica-photo-f572.jpg");
+IRestResponse response = client.Execute(request);
+           Assert.Equal("OK", response.StatusCode.ToString());
+        }
+
+
+    }
+}
